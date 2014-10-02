dba8682577bc4fcbe1a7b13c3002176ef373e6a6
chaminjoo <k22tmfrl@naver.com>
2014-10-02 16:31:56 +0900

    rect

--- sketch.js
+++ sketch.js
@@ -14,16 +14,16 @@ function draw()
    ellipse(400, 400, 400, 400);
 
 if (mouseX < 100) {
-  ellipse(50, 200, 100, 100);
+  rect(50, 200, 100, 100);
 } else if (mouseX < 200){
-  ellipse(150, 200, 100, 100);
+  rect(150, 200, 100, 100);
 } else if (mouseX < 300){
-  ellipse(250, 200, 100, 100);
+  rect(250, 200, 100, 100);
 } else if (mouseX < 400){
-  ellipse(350, 200, 100, 100);
+  rect(350, 200, 100, 100);
 } else if (mouseX < 500){
-  ellipse(450, 200, 100, 100);
+  rect(450, 200, 100, 100);
 } else {
-  ellipse(550, 200, 100, 100);
+ rect(550, 200, 100, 100);
 }
 }
