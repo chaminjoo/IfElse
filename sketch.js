8e553b9c6b6012f03a132fdb2f87e3dcc7f4f46f
chaminjoo <k22tmfrl@naver.com>
2014-10-02 16:19:57 +0900

    ifelse

--- sketch.js
+++ sketch.js
@@ -1,9 +1,17 @@
 function setup() {
   createCanvas(640, 480);
 //background(red, green, blue);
+
+  }
+// draw function
+function draw(){
   background(255, 0, 0);
+if (mouseX < 300) {
+  
   stroke(255);
   noFill();
 
-  ellipse(50, 50, 50, 50);
+  ellipse(200, 200, 100, 100);
+}
+ 
 }
